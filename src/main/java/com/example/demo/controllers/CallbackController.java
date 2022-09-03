fa11858d3e4875a30463735b17a29b83b9bea49b
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-09-03 04:11:12 +0300

    update

--- src/main/java/com/example/demo/controllers/CallbackController.java
+++ src/main/java/com/example/demo/controllers/CallbackController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping ("/")
-public class MainController {
+@RequestMapping("/callback")
+public class CallbackController {
     @GetMapping
-    public String getMainPage(){
-        return "index";
+    public String getCallback(){
+        return ("callback");
     }
 }
--- src/main/java/com/example/demo/controllers/NoController.java
+++ src/main/java/com/example/demo/controllers/NoController.java
@@ -1,14 +1,15 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping ("/")
-public class MainController {
+@RequestMapping("/no")
+public class NoController {
     @GetMapping
-    public String getMainPage(){
-        return "index";
+    public String getNo(){
+        return ("no");
     }
 }
