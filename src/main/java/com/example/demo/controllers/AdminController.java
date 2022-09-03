89b97c62c650af5da515645385019ad834a82af2
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-09-03 04:58:27 +0300

    update

--- src/main/java/com/example/demo/controllers/AdminController.java
+++ src/main/java/com/example/demo/controllers/AdminController.java
@@ -1,19 +1,24 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Admins;
 import com.example.demo.repo.AdminRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/admin")
 public class AdminController {
     @Autowired
     AdminRepo adminRepo;
 
     @GetMapping
-    public String getMessage () {
-        return  ("admin");
+    public List<Admins> getMessage () {
+        List<Admins> admins = adminRepo.findAll();
+       return admins;
     }
 }
