bc512ffc00fe9754a2e82faa9c882129bdd9f745
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-09-03 04:54:14 +0300

    update

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Admins;
+import com.example.demo.repo.AdminRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping ("/")
 public class MainController {
+    @Autowired
+    AdminRepo adminRepo;
     @GetMapping
     public String getMainPage(){
+        Admins admins = new Admins();
+        admins.setName("admin");
+        admins.setPassword("admin");
+        adminRepo.save(admins);
         return "index";
     }
 }
